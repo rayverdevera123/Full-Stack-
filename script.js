3f5eb93af887922d0b5c20c88e54eef07731c787
rayverdevera123 <rayverdevera80@gmail.com>
2026-05-15 15:45:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+    output.innerHTML = "Frontend Button Clicked!";
+});
